096c16aa87c573641e38b51690f6c664ea218b15
MaxPacheco02 <a01552369@tec.mx>
2024-03-04 16:39:23 -0600

    Changes

--- xarm_planner/src/xarm_follow_pose.cpp
+++ xarm_planner/src/xarm_follow_pose.cpp
@@ -1,19 +1,23 @@
 #include "xarm_planner/xarm_planner.h"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "nav_msgs/msg/path.hpp"
 
 using namespace std::chrono_literals;
 
-geometry_msgs::msg::Pose target_pose;
+nav_msgs::msg::Path path;
 const std::string x_name = "xarm6";
 rclcpp::Node::SharedPtr node = nullptr;
 xarm_planner::XArmPlanner planner;
 
-void update(const geometry_msgs::msg::Pose & msg){
-    planner.planPoseTarget(msg);
-    planner.executePath();
-    RCLCPP_INFO(node->get_logger(), "Goal: %f, %f, %f", 
-    msg.position.x, msg.position.y, msg.position.z);
+void update(const nav_msgs::msg::Path & msg){
+    for(int i = 0 ; i < msg.poses.size() ; i++){
+        RCLCPP_INFO(node->get_logger(), "Goal: %f, %f, %f", 
+        msg.poses[i].pose.position.x, msg.poses[i].pose.position.y, msg.poses[i].pose.position.z);
+        planner.planPoseTarget(msg.poses[i].pose);
+        planner.executePath();
+        rclcpp::sleep_for(150ms);
+    }
 }
 
 int main(int argc, char** argv)
@@ -26,9 +30,9 @@ int main(int argc, char** argv)
 
     RCLCPP_INFO(node->get_logger(), "xarm_follow_pose node start");
 
-    rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr target_pose_sub;
-    target_pose_sub = node->create_subscription<geometry_msgs::msg::Pose>(
-        "/xarm_target", 10, update);
+    rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr target_pose_sub;
+    target_pose_sub = node->create_subscription<nav_msgs::msg::Path>(
+        "/xarm_planned_path", 10, update);
 
     rclcpp::spin(node);
     RCLCPP_INFO(node->get_logger(), "xarm_follow_pose over");
